90444b66006dcd5f76520f1e8f400a1fe733687f
b3reketab <141371508+b3reketab@users.noreply.github.com>
2025-03-27 12:03:32 +0300

    clean file organization

--- src/components/contact/contact.js
+++
@@ -1,7 +0,0 @@
-const adress = document.createElement('p');
-adress.textContent = 'Wolaita Soddo, Ethiopia';
-
-const contactInfo = document.createElement('p');
-contactInfo.textContent = 'Manager +251-904824938';
-
-export { adress, contactInfo }
